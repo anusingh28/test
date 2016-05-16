7f3c7695ccd5172942e44fb50d5a20ebb5efa254
bcjitu <bcjitu@gmail.com>
2016-05-16 21:36:45 +0530

    Create customer.java
    customer detais

---
+++ src/test/customer.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ *
+ * @author sijitend
+ */
+public class Customer {
+
+    String Name;
+    String FatherName;
+    String Mobile;
+    String Vill;
+    String Town;
+    String Dist;
+    String Landmark;
+    int PINCODE;
+    
+    public String getName() {
+        return Name;
+    }
+
+    public String getFatherName() {
+        return FatherName;
+    }
+
+    public String getMobile() {
+        return Mobile;
+    }
+
+    public String getVill() {
+        return Vill;
+    }
+
+    public String getTown() {
+        return Town;
+    }
+
+    public String getDist() {
+        return Dist;
+    }
+
+    public String getLandmark() {
+        return Landmark;
+    }
+
+    public int getPINCODE() {
+        return PINCODE;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setFatherName(String FatherName) {
+        this.FatherName = FatherName;
+    }
+
+    public void setMobile(String Mobile) {
+        this.Mobile = Mobile;
+    }
+
+    public void setVill(String Vill) {
+        this.Vill = Vill;
+    }
+
+    public void setTown(String Town) {
+        this.Town = Town;
+    }
+
+    public void setDist(String Dist) {
+        this.Dist = Dist;
+    }
+
+    public void setLandmark(String Landmark) {
+        this.Landmark = Landmark;
+    }
+
+    public void setPINCODE(int PINCODE) {
+        this.PINCODE = PINCODE;
+    }
+    
+
+    
+}
